fbe949effb5a21af8a2b6671254b8cb40a85c3db
victoriavianx <victoriagarcia963@gmail.com>
2022-08-30 23:34:50 -0300

    feat: product retrieve, update and delete routes

--- server/src/routes/products.routes.ts
+++ server/src/routes/products.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import createProduct from "../controllers/products/createProduct.controller";
+import deleteProduct from "../controllers/products/deleteProduct.controller";
 import listProducts from "../controllers/products/listProducts.controller";
+import listSpecificProduct from "../controllers/products/listSpecificProduct.controller";
+import updateProduct from "../controllers/products/updateProduct.controller";
 
 const productRoute = Router();
 
 productRoute.post("/cadastro", createProduct);
 productRoute.get("/", listProducts);
+productRoute.get("/:id", listSpecificProduct);
+productRoute.patch("/:id", updateProduct);
+productRoute.delete("/:id", deleteProduct);
 
 export default productRoute;

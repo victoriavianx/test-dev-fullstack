daf6d9b6a754321d1c82ab4b9bd343ceb3d1d212
victoriavianx <victoriagarcia963@gmail.com>
2022-09-05 14:38:59 -0300

    feat: global styles config

---
+++ client/src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+   
+    :root {
+        --body-font: 'Poppins', sans-serif;
+        --font-regular: 400;
+        --font-medium: 500;
+        --font-semibold: 600;
+
+        --first-title-color: #EA1A7F;
+        --second-title-color: #FEC603;
+        --third-title-color: #A8F387;
+        --hover-color: #16D6FA;
+    }
+
+    html, body {
+        width: 100%;
+        height: 100vh;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+`;

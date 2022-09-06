654ec70ce37ee670f1d4b6a595cb0d22c238494a
victoriavianx <victoriagarcia963@gmail.com>
2022-09-06 12:54:39 -0300

    feat: button to announce

--- client/src/components/Header/header.tsx
+++ client/src/components/Header/header.tsx
@@ -1,17 +1,20 @@
-import { Container, Logo, Profile } from "./styles";
+import { Button, Container, Content, Logo, Profile } from "./styles";
 import { FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
   return (
     <Container>
-      <Logo>
-        <h1>V</h1>
-        <h1>L</h1>
-        <h1>X</h1>
-      </Logo>
-      <Profile>
-        <FaUserCircle />
-      </Profile>
+      <Content>
+        <Logo>
+          <h1>V</h1>
+          <h1>L</h1>
+          <h1>X</h1>
+        </Logo>
+        <Button>Anunciar</Button>
+        <Profile>
+          <FaUserCircle />
+        </Profile>
+      </Content>
     </Container>
   );
 };
--- client/src/components/Header/styles.ts
+++ client/src/components/Header/styles.ts
@@ -2,11 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.header`
   width: 100%;
-  height: 100px;
+  height: 90px;
+  display: flex;
+  justify-content: center;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 90px;
   display: flex;
   align-items: center;
   justify-content: space-around;
-  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  max-width: 1400px;
 `;
 
 export const Logo = styled.div`
@@ -34,6 +42,36 @@ export const Logo = styled.div`
       color: var(--third-title-color);
     }
   }
+
+  @media (min-width: 700px) {
+    margin-left: 6rem;
+  }
+
+  @media (min-width: 1200px) {
+    margin-left: 10rem;
+  }
+`;
+
+export const Button = styled.button`
+  width: 40vw;
+  height: 30px;
+  max-width: 100px;
+  border: 1px solid var(--contour-color);
+  border-radius: 5px;
+  background-color: transparent;
+  font-family: var(--body-font);
+
+  :hover {
+    transition: 0.4s;
+    background-color: var(--hover-color);
+    border: 1px solid var(--hover-color);
+    color: var(--font-hover-color);
+  }
+
+  @media (min-width: 500px) {
+    height: 34px;
+    margin-right: 2rem;
+  }
 `;
 
 export const Profile = styled.div`
@@ -42,10 +80,19 @@ export const Profile = styled.div`
   svg {
     width: 35px;
     height: 35px;
-    padding: 1rem;
+    padding: 1.2rem 1rem 1rem 1rem;
 
     :hover {
       color: var(--hover-color);
+      transition: 0.4s;
     }
+  }
+
+  @media (min-width: 700px) {
+    margin-right: 6rem;
+  }
+
+  @media (min-width: 1200px) {
+    margin-right: 10rem;
   }
 `;

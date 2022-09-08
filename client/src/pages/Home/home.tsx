bc9d0e9e55c80784b8ec1c72ebb7661bbc301fc1
victoriavianx <victoriagarcia963@gmail.com>
2022-09-08 00:10:15 -0300

    update: main content

--- client/src/pages/Home/home.tsx
+++ client/src/pages/Home/home.tsx
@@ -1,18 +1,13 @@
 import Header from "../../components/Header/header";
-import { Container, Content, Main, TitleBox } from "./styles";
+import ProductShowcase from "../../components/Showcase/showcase";
+import { Main } from "./styles";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Main>
-        <Container>
-          <TitleBox>
-            <h2>Meus produtos</h2>
-          </TitleBox>
-          <hr />
-          <Content></Content>
-        </Container>
+        <ProductShowcase />
       </Main>
     </>
   );

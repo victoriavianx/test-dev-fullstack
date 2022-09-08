2581adbcd9bf060cce3d6ac231f49269c8b1e5f2
victoriavianx <victoriagarcia963@gmail.com>
2022-09-08 13:10:45 -0300

    feat: register product page

--- client/src/pages/Register/register.tsx
+++ client/src/pages/Register/register.tsx
@@ -1,5 +1,18 @@
+import Form from "../../components/Form/form";
+import Header from "../../components/Header/header";
+import { Main } from "./styles";
+
 const RegisterProduct = () => {
-  return <div>Cadastrar produto</div>;
+  return (
+    <>
+      <Header />
+      <Main>
+        <section>
+          <Form />
+        </section>
+      </Main>
+    </>
+  );
 };
 
 export default RegisterProduct;
--- client/src/pages/Register/styles.ts
+++ client/src/pages/Register/styles.ts
@@ -1,1 +1,14 @@
 import styled from "styled-components";
+
+export const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  section {
+    width: 100%;
+    margin: 5rem auto;
+    max-width: 1024px;
+  }
+`;

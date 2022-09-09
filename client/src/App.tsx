c5defb869b6eb24fe801203dfc1571c230c40384
victoriavianx <victoriagarcia963@gmail.com>
2022-09-09 18:27:43 -0300

    feat: toast container from toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Routes from "./routes";
+
 import { GlobalStyles } from "./styles/global";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Routes />
       <GlobalStyles />
     </>
